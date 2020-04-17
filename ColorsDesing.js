92f7f9b179e623478c4bc8aee2e260878a8db731
Miguel_EspinalesEs <mee@topfloormarketing.net>
2020-04-17 15:19:40 -0600

    asfasfas

---
+++ ColorsDesing.js
@@ -0,0 +1,43 @@
+
+const colorPrincipal = {
+  primary: {
+    1: '#9c27b0',
+    2: '#ab47bc',
+    3: '#ba68c8',
+    4: '#ce93d8',
+    5: '#e1bee7'
+  },
+  info: {
+    1: '#00bcd4',
+    2: '#26c6da',
+    3: '#26c6da',
+    4: '#4dd0e1',
+    5: '#80deea '
+  },
+  success: {
+    1: '#4caf50',
+    2: '#66bb6a',
+    3: '#81c784',
+    4: '#a5d6a7',
+    5: '#c8e6c9'
+  },
+  warning: {
+    1: '#ff9800',
+    2: '#ffa726',
+    3: '#ffb74d',
+    4: '#ffcc80',
+    5: '#ffe0b2'
+  },
+  danger: {
+    1: '#f44336',
+    2: '#ef5350',
+    3: '#e57373',
+    4: '#ef9a9a',
+    5: '#ffcdd2'
+  },
+  black: '#000',
+  white: '#fff',
+  Default: '#999999'
+}
+
+module.exports = colorPrincipal
