fe8c8d6f3dbe459feccaca81b09ebb307981489a
Miguel_EspinalesEs <mee@topfloormarketing.net>
2020-04-17 15:19:32 -0600

    prueba

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App () {
     <div className='App'>
       <h1>prueba</h1>
       <div className='App'>
-        <Button type='primary'>Button</Button>
+        <Button className='btn bg-colorPrincipalPrimary-1 hover:bg-colorPrincipalPrimary-2 text-white font-bold py-2 px-4 rounded'>Button</Button>
       </div>
       <div class='h-64'>
         <div class='p-4 m-4 bg-green-600'>
@@ -14,7 +14,9 @@ function App () {
         </div>
         <div class='p-4 m-4 bg-green-300 h-full'>
           <h2 class='text-green-900'>Have much fun using Tailwind CSS</h2>
-          <button class='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>My Tailwind Button</button>
+          <button class='btn bg-colorPrincipalPrimary-1 hover:bg-colorPrincipalPrimary-2 text-white font-bold py-2 px-4 rounded'>
+            Button
+          </button>
         </div>
       </div>
     </div>
