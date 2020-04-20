470fc034826e8a288dbd36c01f297bb8322f1cda
Miguel_EspinalesEs <mee@topfloormarketing.net>
2020-04-20 17:53:01 -0600

    configurando rutas de los layouts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react'
-import Login from './Views/Login'
+
+import AuthLayout from './layouts/Aut'
+
+import AdminLayout from './layouts/Admin'
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from 'react-router-dom'
 
 function App () {
   return (
     <div className='App'>
-      <Login />
+      <Router>
+        <Switch>
+          <Route path='/auth' render={props => <AuthLayout {...props} />} />
+          <Route path='/admin' render={props => <AdminLayout {...props} />} />
+          <Redirect from='/' to='/auth/login' />
+        </Switch>
+      </Router>
     </div>
   )
 }
